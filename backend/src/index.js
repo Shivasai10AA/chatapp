bc1746eeaba5cbd191ccdd8afa8d58a1893145c5
Shivasai10AA <shivasaiqre@gmail.com>
2025-07-06 20:44:24 +0530

    configured monorepo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import "dotenv/config";
+
 import cors from "cors";
+import express from "express";
+import cookieParser from "cookie-parser";
 
 import path from "path";
 
@@ -11,10 +12,8 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-dotenv.config();
-
-const PORT = process.env.PORT;
 const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,7 +21,7 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
 
 app.use("/api/auth", authRoutes);
@@ -31,7 +30,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("*", (_req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
